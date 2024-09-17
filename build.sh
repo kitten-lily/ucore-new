0ea7eecab9658745be7fcafe00e617464d972531
Lily <20535390+kitten-lily@users.noreply.github.com>
2024-09-17 10:14:25 +0000

    Intel Media Driver

--- build.sh
+++ build.sh
@@ -4,6 +4,10 @@ set -ouex pipefail
 
 RELEASE="$(rpm -E %fedora)"
 
+### Add rpmfusion
+
+# Not installed by default on ucore
+rpm-ostree install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 ### Install packages
 
@@ -13,10 +17,11 @@ RELEASE="$(rpm -E %fedora)"
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-rpm-ostree install screen
+# rpm-ostree install screen
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
+rpm-ostree install intel-media-driver
 
 #### Example for enabling a System Unit File
 
