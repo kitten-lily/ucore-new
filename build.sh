e3068e2b53f8061ff982fdd02975823c119ff38d
Lily <20535390+kitten-lily@users.noreply.github.com>
2024-10-31 08:53:27 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,7 +6,10 @@ RELEASE="$(rpm -E %fedora)"
 
 ### Add rpmfusion
 # Not installed by default on ucore
-rpm-ostree install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+rpm-ostree install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(RELEASE).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(RELEASE).noarch.rpm
+
+### Add fish repo
+curl -Lo /etc/yum.repos.d/fish.repo https://download.opensuse.org/repositories/shells:fish/Fedora_$(RELEASE)/shells:fish.repo
 
 ### Install latest rclone to get Proton support
 
@@ -15,10 +18,15 @@ rpm-ostree override replace https://downloads.rclone.org/rclone-current-linux-am
 ### Install packages
 rpm-ostree install intel-media-driver \
     libva libva-utils \
-    fish
+    fish \
+    micro \
+    bat
 
 ### Add Starship
 # Starship Shell Prompt
 curl -Lo /tmp/starship.tar.gz "https://github.com/starship/starship/releases/latest/download/starship-x86_64-unknown-linux-gnu.tar.gz"
 tar -xzf /tmp/starship.tar.gz -C /tmp
 install -c -m 0755 /tmp/starship /usr/bin
+
+### Add chezmoi
+sh -c "$(curl -fsLS get.chezmoi.io)" -- -b /usr/bin
